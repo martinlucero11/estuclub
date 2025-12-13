225d2db38f19eb4c41307c2c3897e7f801f6cb41
Martin Lucero <martinahuelucero2@gmail.com>
2025-12-13 22:51:37 +0000

    En "Icon Type", selecciona Other > Splash Screen Logo. esto no me aparec

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,24 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.estuclub.app',
   appName: 'EstuClub',
-  webDir: 'out'
+  webDir: 'out',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 3000,
+      launchAutoHide: true,
+      backgroundColor: "#D44459",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+      useDialog: true,
+      layoutName: "launch_screen",
+      androidSpinnerStyle: "large",
+      splashSpinnerStyle: 'large',
+      spinnerColor: '#ffffff',
+      showSpinner: true,
+    }
+  }
 };
 
 export default config;
