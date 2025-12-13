24b791f412dad424a516abd5fcc1d6e1e9a1c7ca
Martin Lucero <martinahuelucero2@gmail.com>
2025-12-13 21:35:28 +0000

    Alto. Hay un error técnico crítico en ese mensaje, aunque el razonamient

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.estuclub.app',
   appName: 'EstuClub',
-  webDir: '.next'
+  webDir: 'out'
 };
 
 export default config;
