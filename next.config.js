f9549d831920a0d1a26d17c2ea9856a38bb6b832
Martin Lucero <martinahuelucero2@gmail.com>
2025-12-14 06:07:30 +0000

    Acción mínima para destrabar el rollout
    Tenés que hacer pasar el build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
   output: 'standalone',
   typescript: {
     ignoreBuildErrors: true,
@@ -9,7 +8,6 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
