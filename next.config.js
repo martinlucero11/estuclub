ccbad85ef31c39f773e2700a0630085380b6129c
Martin Lucero <martinahuelucero2@gmail.com>
2025-12-18 16:59:40 +0000

    permiteles todos los host names

--- next.config.js
+++ next.config.js
@@ -6,11 +6,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-       {
-        protocol: 'https',
-        hostname: 'picsum.photos',
+        hostname: '**',
       },
     ],
   },
