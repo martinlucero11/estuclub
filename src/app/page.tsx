fde8922fdc47aa5dadcd674e786931aee0294d6b
Martin Lucero <martinahuelucero2@gmail.com>
2026-02-23 18:17:28 +0000

    Try fixing this error: `Unhandled Runtime Error: TypeError: (0 , _fireba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,10 @@ import { ArrowRight, ChevronRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import MainLayout from '@/components/layout/main-layout';
 import Link from 'next/link';
-import { useCollection, useFirestore, useDocOnce, useMemo } from '@/firebase';
+import { useCollection, useFirestore, useDocOnce } from '@/firebase';
 import { collection, query, where, limit, doc, orderBy } from 'firebase/firestore';
 import type { Banner, Category, HomeSection } from '@/lib/data';
-import { useRef } from 'react';
+import { useRef, useMemo } from 'react';
 import { Skeleton } from '@/components/ui/skeleton';
 import Image from 'next/image';
 import WelcomeMessage from '@/components/home/welcome-message';
@@ -268,3 +268,5 @@ export default function HomePage() {
     </MainLayout>
   );
 }
+
+    
