bdfdf8feb1d783476d55b9b65288dc174a720522
Martín <martinahuelucero2@gmail.com>
2026-03-30 19:42:17 -0300

    fix: skip ts/eslint checks to prevent Out of Memory in App Hosting

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   output: process.env.CAPACITOR_BUILD === 'true' ? 'export' : undefined,
   images: {
     unoptimized: true,
